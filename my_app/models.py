5d2ce93427c2b894df5453d372072198249fb6b0
sibonisonkosi <sibonisonkosi@gmail.com>
2020-01-13 14:11:21 +0200

    cart count

--- my_app/models.py
+++ my_app/models.py
@@ -36,6 +36,10 @@ class Item(models.Model):
         return reverse("my_app:add-to-cart", kwargs={
             'slug': self.slug
         })
+    def get_remove_from_cart_url(self):
+        return reverse("my_app:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
 
 class OrderItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- my_app/urls.py
+++ my_app/urls.py
@@ -5,5 +5,6 @@ app_name = "my_app"
 urlpatterns =[
     path('', views.defaultView.as_view(), name = 'default'),
     path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart')
 ]
